d0b258b31ed8cacc858f4b579db344d4b6f0cf9b
AsusOnePlus X <onex@AsusOnePlusX.local>
2020-08-11 16:39:53 -0500

    solving exercise

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    value: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      value: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input onChange={this.handleChange} type="text" placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.value}</p>
       </div>
     );
   }
